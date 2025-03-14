e791dc3be8c8c41be06325ce919b78316e03bded
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-03-14 11:27:09 +0200

    model now accepts preprocessed image

--- app/logic/model_logic.py
+++ app/logic/model_logic.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
 from PIL import Image
 
-def get_model_prediction(image_loc, model_link):
+def get_model_prediction(image, model_link):
 
-    img_test = Image.open(image_loc)
     model = YOLO(model_link)
-    results = model.predict(img_test)
+    results = model.predict(image)
     result = results[0].cpu()
 
     return result
